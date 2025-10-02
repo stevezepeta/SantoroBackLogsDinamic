1610a67e6d7bb7630849dd9ce1e453be92da9aa3
stevezepeta <salvarez@gruposantoro.com>
2025-10-02 13:02:31 -0600

    Primer commit

---
+++ src/main/java/backlogs/dinamico/BacklogsApplication.java
@@ -0,0 +1,13 @@
+package backlogs.dinamico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacklogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backlogs/dinamico/BacklogsApplicationTests.java
@@ -0,0 +1,13 @@
+package backlogs.dinamico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacklogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
