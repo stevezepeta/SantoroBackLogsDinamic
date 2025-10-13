76efeca48ae001924d66ef28e2597ec51753338a
stevezepeta <salvarez@gruposantoro.com>
2025-10-13 14:07:35 -0600

    Tablas con BD

--- src/main/java/backlogs/dinamico/model/core/Organization.java
+++ src/main/java/backlogs/dinamico/model/core/Organization.java
@@ -13,13 +13,11 @@ import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data @NoArgsConstructor @AllArgsConstructor @SuperBuilder
 @org.springframework.data.mongodb.core.mapping.Document(collection = "organizations")
-@CompoundIndexes({
-@CompoundIndex(name = "ux_org_domain", def = "{ 'domain': 1 }", unique = true),
-@CompoundIndex(name = "ix_org_status", def = "{ 'status': 1 }")
+@CompoundIndexes({ @CompoundIndex(name = "ix_org_status", def = "{ 'status': 1 }")
 })
 public class Organization extends BaseEntity {
 private String name; // Nombre comercial
-@Indexed(unique = true)
+@Indexed(name = "ux_org_domain", unique = true)
 private String domain; // Dominio principal
 private String status; // active|disabled
 private Document settings; // Retención, zona horaria, límites, etc.
